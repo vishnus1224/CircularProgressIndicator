317544fcf8ea74d0d629ccafff5157578bb8e9c5
Vishnu Shirodkar <vishnus1224@gmail.com>
2015-10-07 19:33:32 +0530

    empty project commit

---
+++ Project/app/src/main/java/com/vishnus1224/circularprogressindicatorexample/MainActivity.java
@@ -0,0 +1,16 @@
+package com.vishnus1224.circularprogressindicatorexample;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
