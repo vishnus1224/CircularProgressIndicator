4a9a8e9ad621c97133d859b76e6304a2a199255c
Vishnu Shirodkar <vishnus1224@gmail.com>
2015-10-07 20:54:38 +0530

    added buttons for increasing and decreasing progress

--- Project/example/src/main/java/com/vishnus1224/circularprogressindicatorexample/MainActivity.java
+++ Project/example/src/main/java/com/vishnus1224/circularprogressindicatorexample/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.vishnus1224.circularprogressindicator.CircularProgressIndicator;
 
@@ -11,12 +13,32 @@ public class MainActivity extends ActionBarActivity {
 
     private CircularProgressIndicator circularProgressIndicator;
 
+    private Button increaseProgressButton;
+    private Button decreaseProgressButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         circularProgressIndicator = (CircularProgressIndicator) findViewById(R.id.circularProgress);
+
+        increaseProgressButton = (Button) findViewById(R.id.increaseProgressButton);
+        decreaseProgressButton = (Button) findViewById(R.id.decreaseProgressButton);
+
+        increaseProgressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                circularProgressIndicator.animateProgress(1000, 95);
+            }
+        });
+
+        decreaseProgressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                circularProgressIndicator.animateProgress(1000, 20);
+            }
+        });
     }
 
 }
